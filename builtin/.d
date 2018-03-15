704e0434f11f39b606e09dd96ea03e2a1526e20d
john-uwc <john.uwc@outlook.com>
2018-03-15 17:18:00 +0800

    refact builtin to form a cl tool set

--- builtin/.d
+++ builtin/.d
@@ -7,4 +7,4 @@ _include "util/pair.sh"
 # handle with normal change directory routine
 _invoke_2c "unalias" "cd"
 eb=$(pwd); cd "$@"; ea=$(pwd); _dw_store "cd" $(pair new "$ea" "$eb")
-_invoke_2c "alias" "cd=source $(_root)/cd.sh"
+_invoke_2c "alias" "cd=source $(_root)/builtin/.d"
