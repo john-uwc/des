65dac0c43b8845e56bde57c766588a8f6dc8d5cf
john-uwc <john.uwc@outlook.com>
2018-03-16 17:22:50 +0800

    fix variable definition error

--- builtin/.d
+++ builtin/.d
@@ -6,5 +6,5 @@ _include "util/pair.sh"
 
 # handle with normal change directory routine
 _invoke_2c "unalias" "cd"
-local eb=$(pwd); cd "$@"; local ea=$(pwd); _dw_store "d" $(pair new "$ea" "$eb")
+declare eb=$(pwd); cd "$@"; declare ea=$(pwd); _dw_store "d" $(pair new "$ea" "$eb")
 _invoke_2c "alias" "cd=source .d"
