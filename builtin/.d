d738f0a6e289245fda70e02f3bb43c20b013a9ef
john-uwc <john.uwc@outlook.com>
2018-03-16 14:12:18 +0800

    fix issue of builtin invoke

--- builtin/.d
+++ builtin/.d
@@ -6,5 +6,5 @@ _include "util/pair.sh"
 
 # handle with normal change directory routine
 _invoke_2c "unalias" "cd"
-eb=$(pwd); cd "$@"; ea=$(pwd); _dw_store "cd" $(pair new "$ea" "$eb")
-_invoke_2c "alias" "cd=source $(_root)/builtin/.d"
+local eb=$(pwd); cd "$@"; local ea=$(pwd); _dw_store "d" $(pair new "$ea" "$eb")
+_invoke_2c "alias" "cd=source .d"
